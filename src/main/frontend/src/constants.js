aed3836fb6d824aff5fc8b18fc35d19e00ef3739
박소정 <hhhyun913@gmail.com>
2025-08-09 05:07:01 +0900

    Re-push constants.js with minor update

--- src/main/frontend/src/constants.js
+++ src/main/frontend/src/constants.js
@@ -1,8 +1,9 @@
 import { Platform } from "react-native";
+import Constants from "expo-constants";
 
 const API_BASE_URL =
   Platform.OS === "web"
-    ? "http://localhost:8080"
-    : "http://192.168.219.101:8080"; // ✅ 모바일은 IP 주소 사용
+    ? "http://localhost:8080" // 웹 개발 시 로컬 API
+    : Constants.expoConfig.extra.API_BASE_URL; // 모바일에서는 .env 값
 
 export { API_BASE_URL };
